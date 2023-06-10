02d401b2744834fa92cf2ac03cf1bf12c2f0f0b8
kerfootj <jkerfoot@paystone.com>
2023-06-10 09:24:41 -0700

    check env for prod, fallback to dev

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -16,7 +16,11 @@ if (!process.env.MONGODB_URI) {
     throw new Error('Please add your Mongo URI to .env.local');
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+} else {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     // @ts-expect-error: globalThis is untyped
@@ -27,10 +31,6 @@ if (process.env.NODE_ENV === 'development') {
     }
     // @ts-expect-error: globalThis is untyped
     clientPromise = global._mongoClientPromise;
-} else {
-    // In production mode, it's best to not use a global variable.
-    client = new MongoClient(uri, options);
-    clientPromise = client.connect();
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
